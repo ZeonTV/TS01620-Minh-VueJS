ce59f3f599c20e1110453f13c1859c434ce283ac
ZeonTV <zeontv1806@gmail.com>
2026-01-19 19:58:31 +0700

    lab4

---
+++ LAB4/src/components/Bai3.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ greeting }}</h1>
+    
+    <button v-bind:title="buttonTitle" v-on:click="changeGreeting">
+      Click để thay đổi
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Chào cậu nha :3!',
+      buttonTitle: 'Nhấn để thay đổi lời chào'
+    };
+  },
+  methods: {
+    changeGreeting() {
+      if (this.greeting === 'Chào cậu nha :3!') {
+        this.greeting = 'Chúc ngày mới tốt lành';
+        this.buttonTitle = 'Lời chào mới đã được cập nhật';
+      } else {
+        this.greeting = 'Chào cậu nha :3!';
+        this.buttonTitle = 'Nhấn để thay đổi lời chào';
+      }
+    }
+  }
+}
+</script>
---
+++ LAB4/src/components/Directives.vue
@@ -0,0 +1,24 @@
+<template>
+    <p>{{ message }}</p>
+        <button v-on:click="Title"></button>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            Title:"hide"
+                }
+            },
+    methods: {
+        Test() {
+            console.log(this.Title);
+            if (this.Title === "hide") {
+                this.Title = "show";
+                return
+            }
+            this.Title ="hide";
+        }
+                            }
+                }
+                    
+</script>
