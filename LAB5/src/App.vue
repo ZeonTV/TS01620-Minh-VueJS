1e700c7287dd329f93f46a5e5bad69109210b3d0
ZeonTV <zeontv1806@gmail.com>
2026-01-26 22:37:16 +0700

    bai2

--- LAB5/src/App.vue
+++ LAB5/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-import bai1 from './components/bai1.vue';
+// import bai1 from './components/bai1.vue';
+import bai2 from './components/bai2.vue';
 </script>
 
 <template>
-<bai1/>
+<!-- <bai1/> -->
+<bai2/>
 </template>
 
 <style scoped>
