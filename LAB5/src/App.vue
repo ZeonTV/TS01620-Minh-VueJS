e9ad619b37367103e2fddfc64fa04aa3389cfb89
ZeonTV <zeontv1806@gmail.com>
2026-01-26 23:02:13 +0700

    bai4

--- LAB5/src/App.vue
+++ LAB5/src/App.vue
@@ -1,13 +1,28 @@
-<script setup>
-// import bai1 from './components/bai1.vue';
-import bai2 from './components/bai2.vue';
-</script>
-
 <template>
-<!-- <bai1/> -->
-<bai2/>
+  <div id="app">
+    <h1>Ứng dụng Blog nhỏ với Vue.js</h1>
+    <CreatePost @add-post="addPost" />
+    <PostList :posts="posts" />
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import { ref } from 'vue';
+import CreatePost from './components/CreatePost.vue'; 
+import PostList from './components/PostList.vue';
 
+const posts = ref([]);
+
+function addPost(post) {
+  posts.value.push(post);
+}
+</script>
+
+<style>
+#app {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  font-family: Arial, sans-serif;
+}
 </style>
